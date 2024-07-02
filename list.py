afb138149daf6327fb3b169740b4e0a6da416d88
pokemonjiwoo <ziwo0o@naver.com>
2024-07-02 16:23:28 +0900

    DateType
    string, list, dictionary, boolean 을 배우고 학습함

---
+++ list.py
@@ -0,0 +1,26 @@
+jiwoo = ["이지우", '20', '010-4212-****']
+name = jiwoo[0]
+age = jiwoo[1]
+phone = jiwoo[2]
+print(type(jiwoo))
+print(name, age, phone)
+
+
+names=(['강수경', '강해나', '김민석'], ['20', '21', '22'], ['010-****-****', '010-****-****', '010-****-****'])
+print(names[0][0:2])
+
+
+numbers = [1, 2, 3, 4, 5]
+result = numbers[2] + numbers[-1]
+print(result)
+
+print(len(names[0]))
+
+
+ # 리스트에 요소 추가하기
+last = []
+last.append(30)
+print(last)
+last.remove(3)
+print(type(last))
+
