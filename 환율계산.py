e3e4d27722ee4efeab653702aa638d52062d1077
pokemonjiwoo <ziwo0o@naver.com>
2024-07-02 17:28:16 +0900

    퀴즈1

---
+++ 환율계산.py
@@ -0,0 +1,5 @@
+exchange = {'달러' : 1320, '엔화': 800, '위안': 130}
+chul = [13, 200, 13]
+
+print(exchange['달러'] * chul[0] + exchange['엔화']*chul[1] + exchange['위안'] * chul[2])
+print('가지고 계신 돈은', 'total', '원 입니다.')
---
+++ 회원가입.py
@@ -0,0 +1,20 @@
+#홍길동
+name1 = input("이름을 입력하세요.")
+age1 = input("나이를 입력하세요.")
+email1 = input("나이를 입력하세요.")
+phone1  = input("전화번호를 입력하세요.")
+
+#김철수
+name2 = input("이름을 입력하세요.")
+age2 = input("나이를 입력하세요.")
+email2 = input("나이를 입력하세요.")
+phone2  = input("전화번호를 입력하세요.")
+
+
+
+dic_1 = {'홍길동' : {'age': 26, 'email' : '***', 'phone': '010-9999-****'}}
+dic_2 = {'김철수' : {'age': 30, 'email' : '***',  'phone': '010-7777-****'}}
+print(dic_1)
+print(dic_2)
+
+
